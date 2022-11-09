777a066c945e2027746b7e3029844ea21f3a902d
Tom Collings <tcollings@tcollings-a02.vmware.com>
2022-11-09 11:44:36 -0700

    Initial commit

---
+++ legacy/src/test/java/com/vmware/testharness/legacy/LegacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.testharness.legacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
