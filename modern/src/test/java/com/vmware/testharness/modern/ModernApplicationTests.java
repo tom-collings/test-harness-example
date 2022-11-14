6b453041e2dcdb69c94d06299ab2541205e2833d
tom-collings <tcollings@tcollings-a02.vmware.com>
2022-11-14 08:41:01 -0700

    adding modern app

---
+++ modern/src/test/java/com/vmware/testharness/modern/ModernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.testharness.modern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
